f4ea10164658a59190f9d61a411d5c6fabcc9759
Vanderlândio Z. da Rocha <vandodev@gmail.com>
2022-05-05 20:23:34 -0300

    Projeto com alterações propostas pelo Igor

--- projeto/app.js
+++ projeto/app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var app = express();
 
-//Conexao com banco
-require('./database')
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,23 +20,25 @@ app.use('/', require('./routes/index'));
 
 //ROTAS CRIADAS
 app.use('/login', require('./routes/login'));
+//alteração rota produtos
 app.use('/produtos', require('./routes/produtos'));
+
 app.use('/carrinho', require('./routes/carrinho'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- projeto/database/index.js
+++
@@ -1,8 +0,0 @@
-//Concexão com o banco
-const Sequelize = require('sequelize');
-const configDB = require('../config/database');
-
-const conexao = new Sequelize(configDB);
-
-
-module.exports = conexao;
